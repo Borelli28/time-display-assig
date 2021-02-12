91721dce5bc64f69b0263fdf68c5943fe8616918
armando <armando@email.com>
2021-02-12 12:21:49 -0600

    Index.html display local time

--- time_display/time_display_app/views.py
+++ time_display/time_display_app/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render, HttpResponse
-
+from django.shortcuts import render
+from time import localtime, strftime
+    
 
 def index(request):
-    return HttpResponse("Hello this is your new project")
-
+    context = {
+        "time": strftime("%Y-%m-%d %H:%M %p", localtime())
+    }
+    return render(request,'index.html', context)
